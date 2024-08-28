f9a7213f751d4129b42329b0c9c2cef2a596f0cc
ojan <milyasa2468@gmail.com>
2024-08-28 21:19:35 +0700

    fixing same site

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   app.enableCors({
     allowedHeaders:
       'Origin, X-Requested-With, Content-Type, Accept, Authentication, Refresh, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
-    origin: ['http://localhost:3004', 'https://plants-ecommerce.vercel.app'],
+    origin: 'https://plants-ecommerce.vercel.app',
     credentials: true,
     methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
   });
