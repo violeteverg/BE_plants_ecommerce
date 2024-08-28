1b2ef58c029110fa4d69ffc635b4267136e6ad96
ojan <milyasa2468@gmail.com>
2024-08-28 21:14:21 +0700

    fixing same site

--- src/main.ts
+++ src/main.ts
@@ -10,9 +10,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    allowedHeaders: ['content-type'],
+    allowedHeaders:
+      'Origin, X-Requested-With, Content-Type, Accept, Authentication, Refresh, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
     origin: ['http://localhost:3004', 'https://plants-ecommerce.vercel.app'],
     credentials: true,
+    methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
   });
 
   app.useGlobalPipes(new ValidationPipe());
