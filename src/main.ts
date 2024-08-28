9a3d64d66cb66fb4953d78b55ab27d36f8775543
ojan <milyasa2468@gmail.com>
2024-08-28 21:38:25 +0700

    fixing same site

--- src/main.ts
+++ src/main.ts
@@ -8,30 +8,14 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // Set CORS configuration
   app.enableCors({
-    allowedHeaders: [
-      'Origin',
-      'X-Requested-With',
-      'Content-Type',
-      'Accept',
-      'Authorization',
-      'Authentication',
-      'Refresh',
-    ],
-    origin: 'https://plants-ecommerce.vercel.app',
+    allowedHeaders: ['content-type'],
+    origin: 'https://plants-ecommerce.vercel.app/',
     credentials: true,
-    methods: ['GET', 'HEAD', 'PUT', 'POST', 'DELETE', 'OPTIONS', 'PATCH'],
   });
 
-  // Apply cookie parser middleware
   app.use(cookieParser());
-
-  // Apply validation pipe globally
   app.useGlobalPipes(new ValidationPipe());
-
-  // Start the application
   await app.listen(3007);
 }
 bootstrap();
