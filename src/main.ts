bf0c33e153992a86e0ab44be31504b64953032ff
ojan <milyasa2468@gmail.com>
2024-08-28 21:24:59 +0700

    fixing same site

--- src/main.ts
+++ src/main.ts
@@ -8,16 +8,30 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieParser());
+
+  // Set CORS configuration
   app.enableCors({
-    allowedHeaders:
-      'Origin, X-Requested-With, Content-Type, Accept, Authentication, Refresh, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
+    allowedHeaders: [
+      'Origin',
+      'X-Requested-With',
+      'Content-Type',
+      'Accept',
+      'Authorization',
+      'Authentication',
+      'Refresh',
+    ],
     origin: 'https://plants-ecommerce.vercel.app',
     credentials: true,
-    methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
+    methods: ['GET', 'HEAD', 'PUT', 'POST', 'DELETE', 'OPTIONS', 'PATCH'],
   });
 
+  // Apply cookie parser middleware
+  app.use(cookieParser());
+
+  // Apply validation pipe globally
   app.useGlobalPipes(new ValidationPipe());
+
+  // Start the application
   await app.listen(3007);
 }
 bootstrap();
