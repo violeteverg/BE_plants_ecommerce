c5e549f692792ed5e154b91bb40779797d13bb1e
ojan <milyasa2468@gmail.com>
2024-08-28 21:47:59 +0700

    fixing same site

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     allowedHeaders: ['content-type'],
-    origin: 'https://plants-ecommerce.vercel.app/',
+    origin: [
+      'https://plants-ecommerce.vercel.app/',
+      'https://plants-ecommerce-3r3simab2-violetevergs-projects.vercel.app/',
+    ],
     credentials: true,
   });
 
