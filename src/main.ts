6ec4c93dba5d22d419772d7564b1eb2963a61092
ojan <milyasa2468@gmail.com>
2024-09-13 16:27:58 +0700

    fixing cors

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
 
+  // app.enableCors({
+  //   origin: 'http://localhost:3004',
+  //   allowedHeaders: ['Content-Type', 'Authorization'],
+  //   credentials: true,
+  // });
   app.enableCors({
     origin: '*',
     methods: 'GET, PUT, POST, DELETE',
     allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
   });
 
   app.useGlobalPipes(new ValidationPipe());
